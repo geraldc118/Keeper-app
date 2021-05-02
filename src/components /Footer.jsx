6f092b20c4854e0abab078cbcb2af00b1bbb21dd
geraldc118 <geraldcameron118@gmail.com>
2021-05-02 11:59:09 +0100

    Pycharm update + adding all files

---
+++ src/components /Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+const year = new Date().getFullYear();
+
+function Footer() {
+ return ( <footer>
+    <p>Copyright © {year} </p>
+    </footer>
+);
+}
+
+export default Footer;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./components /App.jsx"
+
+
+ReactDom.render(<App />,document.getElementById("root")
+);
+
+
+
+// Use this as a template for creating React apps.
+// Make sure that the components are wrapped in () when using the return as items may not show sometimes
